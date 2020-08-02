8905d7be1abc0b96d4b201c59c6612f957c97bb0
vipin7974 <viprock234@gmail.com>
2020-08-03 01:01:56 +0530

    buid

---
+++ build/precache-manifest.f5490da82b1110291c733991c14656f8.js
@@ -0,0 +1,326 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac42bc31900daf7e636987d146c7b31",
+    "url": "/archivity/index.html"
+  },
+  {
+    "revision": "9fa4d3e417b691787b9d",
+    "url": "/archivity/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "9f02cbb5529325ed29b9",
+    "url": "/archivity/static/css/main.81ba28c6.chunk.css"
+  },
+  {
+    "revision": "9fa4d3e417b691787b9d",
+    "url": "/archivity/static/js/2.910b2462.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/archivity/static/js/2.910b2462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f02cbb5529325ed29b9",
+    "url": "/archivity/static/js/main.ef44e659.chunk.js"
+  },
+  {
+    "revision": "993cccb07f276593aceb",
+    "url": "/archivity/static/js/runtime-main.d3150398.js"
+  },
+  {
+    "revision": "1bd3f77a17c7e8437b668aab092f90dd",
+    "url": "/archivity/static/media/1565106389512.1bd3f77a.jpg"
+  },
+  {
+    "revision": "4d904c3b8658be002f15b7c5fb0072b7",
+    "url": "/archivity/static/media/PicsArt_04-02-02.17.37.4d904c3b.jpg"
+  },
+  {
+    "revision": "05b047dd3147568f705d7911fa1be9c2",
+    "url": "/archivity/static/media/PicsArt_05-25-01.09.13.05b047dd.png"
+  },
+  {
+    "revision": "d3527b48a47a93645bcebe8a97ed6eb0",
+    "url": "/archivity/static/media/PicsArt_06-07-12.31.11.d3527b48.jpg"
+  },
+  {
+    "revision": "44118fd6572660a3482fc856fb06b12a",
+    "url": "/archivity/static/media/PicsArt_06-11-12.02.11.44118fd6.jpg"
+  },
+  {
+    "revision": "c04935bba6bdfcdf68e80f154e434c39",
+    "url": "/archivity/static/media/PicsArt_06-15-11.05.46.c04935bb.jpg"
+  },
+  {
+    "revision": "4827dcbd7d7b5d06c4d3aeead4d40afd",
+    "url": "/archivity/static/media/PicsArt_06-17-08.06.03.4827dcbd.jpg"
+  },
+  {
+    "revision": "933f79c3b31af4c308900c8a962c5aba",
+    "url": "/archivity/static/media/PicsArt_06-19-11.54.02 (1).933f79c3.jpg"
+  },
+  {
+    "revision": "669c0ce3f6bb5a640143ba59379cb528",
+    "url": "/archivity/static/media/PicsArt_06-22-07.17.13.669c0ce3.jpg"
+  },
+  {
+    "revision": "f481d5a3be0d3d993a0b073ec3d1bf5f",
+    "url": "/archivity/static/media/PicsArt_06-24-10.08.07.f481d5a3.jpg"
+  },
+  {
+    "revision": "8030fac2807ae864376053b47f2e8339",
+    "url": "/archivity/static/media/PicsArt_06-30-09.11.27.8030fac2.jpg"
+  },
+  {
+    "revision": "a456500b05a6d2b9125cc881d153239e",
+    "url": "/archivity/static/media/PicsArt_07-03-05.13.24.a456500b.jpg"
+  },
+  {
+    "revision": "6769a100d8abb39f0b69355e96cc7831",
+    "url": "/archivity/static/media/PicsArt_07-20-04.45.05.6769a100.jpg"
+  },
+  {
+    "revision": "f6724c78c4a3db65dde9abdbe05633ef",
+    "url": "/archivity/static/media/a1.f6724c78.jpg"
+  },
+  {
+    "revision": "0fae36324b7b64fbada273f34accb0cd",
+    "url": "/archivity/static/media/a10.0fae3632.jpg"
+  },
+  {
+    "revision": "bea098627f1ff7c386a26bc77fb18aa8",
+    "url": "/archivity/static/media/a11.bea09862.jpg"
+  },
+  {
+    "revision": "1bd3f77a17c7e8437b668aab092f90dd",
+    "url": "/archivity/static/media/a12.1bd3f77a.jpg"
+  },
+  {
+    "revision": "9f3a011a613af91f3e6872a79d92ad23",
+    "url": "/archivity/static/media/a13.9f3a011a.jpg"
+  },
+  {
+    "revision": "3a04b16230df30e0c85317887e633f2c",
+    "url": "/archivity/static/media/a14.3a04b162.jpg"
+  },
+  {
+    "revision": "4827dcbd7d7b5d06c4d3aeead4d40afd",
+    "url": "/archivity/static/media/a15.4827dcbd.jpg"
+  },
+  {
+    "revision": "933f79c3b31af4c308900c8a962c5aba",
+    "url": "/archivity/static/media/a16.933f79c3.jpg"
+  },
+  {
+    "revision": "e667ed23d0bf79a5402b1a67c9fb26ee",
+    "url": "/archivity/static/media/a17.e667ed23.jpg"
+  },
+  {
+    "revision": "f4c7346f6ca596b26abf59d680aea100",
+    "url": "/archivity/static/media/a18.f4c7346f.jpg"
+  },
+  {
+    "revision": "bac12bfbe6376680863302841d9f147f",
+    "url": "/archivity/static/media/a19.bac12bfb.jpg"
+  },
+  {
+    "revision": "aa733f6a6011a97eb2eb845f660099ee",
+    "url": "/archivity/static/media/a2.aa733f6a.jpg"
+  },
+  {
+    "revision": "3738d2875abac3713d27fa552f62fe4a",
+    "url": "/archivity/static/media/a20.3738d287.jpg"
+  },
+  {
+    "revision": "028cb9bb5ab1da1722b7d9d8b0631ab2",
+    "url": "/archivity/static/media/a21.028cb9bb.png"
+  },
+  {
+    "revision": "064dbb718906f60a82c8a55f983c35b3",
+    "url": "/archivity/static/media/a22.064dbb71.jpg"
+  },
+  {
+    "revision": "13f6c7e12f1c747d6670d60d7fa14ada",
+    "url": "/archivity/static/media/a23.13f6c7e1.jpg"
+  },
+  {
+    "revision": "633fa3d07d60184750f3227b9413994b",
+    "url": "/archivity/static/media/a24.633fa3d0.jpg"
+  },
+  {
+    "revision": "cae913aaaea67136c66be84e855612cf",
+    "url": "/archivity/static/media/a25.cae913aa.jpg"
+  },
+  {
+    "revision": "5099afe54b76f358a4a2a6dece6b6ee9",
+    "url": "/archivity/static/media/a26.5099afe5.jpg"
+  },
+  {
+    "revision": "427a61e0fd3f1f18d0a3f2e9aeb1d0ab",
+    "url": "/archivity/static/media/a27.427a61e0.jpg"
+  },
+  {
+    "revision": "da4bdf056fb7123b9bbcaeb8c3ec21d6",
+    "url": "/archivity/static/media/a28.da4bdf05.jpg"
+  },
+  {
+    "revision": "02c349e80ccf6af08a1897ee37189da5",
+    "url": "/archivity/static/media/a29.02c349e8.jpg"
+  },
+  {
+    "revision": "5482e3abf5737480b3eeda944afec65c",
+    "url": "/archivity/static/media/a3.5482e3ab.jpg"
+  },
+  {
+    "revision": "cb2eb3e244922dfce5ca8ab3a740f209",
+    "url": "/archivity/static/media/a30.cb2eb3e2.jpg"
+  },
+  {
+    "revision": "0aa892705f6f49d95869a22cfab38052",
+    "url": "/archivity/static/media/a4.0aa89270.jpg"
+  },
+  {
+    "revision": "7d60f797659e3507481e703d5d887fc6",
+    "url": "/archivity/static/media/a5.7d60f797.jpg"
+  },
+  {
+    "revision": "03e4c2cc05d2659feddbb02a646015aa",
+    "url": "/archivity/static/media/a6.03e4c2cc.jpg"
+  },
+  {
+    "revision": "fddac8c1cfaba9075b829d7b0b1aae63",
+    "url": "/archivity/static/media/a7.fddac8c1.jpg"
+  },
+  {
+    "revision": "b472b071582e6aba849809b2efa23e94",
+    "url": "/archivity/static/media/a8.b472b071.jpg"
+  },
+  {
+    "revision": "b1db3856f3bae3b4e5b54ade1de7f7e8",
+    "url": "/archivity/static/media/a9.b1db3856.jpg"
+  },
+  {
+    "revision": "a6daab34442dc5bb68ac6fad4b42aaa7",
+    "url": "/archivity/static/media/adever.a6daab34.jpg"
+  },
+  {
+    "revision": "f61a2a7e5a164d4e9b9b16451839e8b9",
+    "url": "/archivity/static/media/arc.f61a2a7e.png"
+  },
+  {
+    "revision": "c04935bba6bdfcdf68e80f154e434c39",
+    "url": "/archivity/static/media/arc1.c04935bb.jpg"
+  },
+  {
+    "revision": "b5cbfb432b474389bf522392fe9a7fca",
+    "url": "/archivity/static/media/arc2.b5cbfb43.jpg"
+  },
+  {
+    "revision": "6769a100d8abb39f0b69355e96cc7831",
+    "url": "/archivity/static/media/arc3.6769a100.jpg"
+  },
+  {
+    "revision": "3eee21d1c6b0a956a616a9b45e260a42",
+    "url": "/archivity/static/media/arc4.3eee21d1.png"
+  },
+  {
+    "revision": "cf6dd9837c7ade85e12e1e428e75a10e",
+    "url": "/archivity/static/media/arc5.cf6dd983.jpg"
+  },
+  {
+    "revision": "8c72593d14733d0fe9d30ecd45ad16ea",
+    "url": "/archivity/static/media/archi.8c72593d.png"
+  },
+  {
+    "revision": "f3d6a6b9a1d4c377458988727af213f2",
+    "url": "/archivity/static/media/archiry_round_logo.f3d6a6b9.png"
+  },
+  {
+    "revision": "19c3640bc11723a9e848ba39323e0566",
+    "url": "/archivity/static/media/archivity_sqr_logo.19c3640b.png"
+  },
+  {
+    "revision": "dec98f88d518a07b3c43d71eda2617ed",
+    "url": "/archivity/static/media/custom – 2.dec98f88.png"
+  },
+  {
+    "revision": "f844fafe1452cfd3dbb1487d239319c0",
+    "url": "/archivity/static/media/g1.f844fafe.jpg"
+  },
+  {
+    "revision": "669c0ce3f6bb5a640143ba59379cb528",
+    "url": "/archivity/static/media/g2.669c0ce3.jpg"
+  },
+  {
+    "revision": "7ae5328210492f49fa5bf41fb329a4df",
+    "url": "/archivity/static/media/g3.7ae53282.png"
+  },
+  {
+    "revision": "a6e4b5e74371d795bc9f8d24efa7eba1",
+    "url": "/archivity/static/media/g4.a6e4b5e7.jpg"
+  },
+  {
+    "revision": "87ed84fae9be7579e9d9b629fa59b297",
+    "url": "/archivity/static/media/graphic.87ed84fa.png"
+  },
+  {
+    "revision": "f44d3815a087abe5bb8577a18d82e950",
+    "url": "/archivity/static/media/image.f44d3815.jpg"
+  },
+  {
+    "revision": "41f52fa964b44693448718c52dc4b104",
+    "url": "/archivity/static/media/insta_c.41f52fa9.png"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/archivity/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "bc43366c8cd5c969550789bf3cca1c7c",
+    "url": "/archivity/static/media/p1.bc43366c.jpg"
+  },
+  {
+    "revision": "a4588f19bf51677d786523a0eed0b4d4",
+    "url": "/archivity/static/media/p10.a4588f19.jpg"
+  },
+  {
+    "revision": "d858576c73a088997c13485d7be292e2",
+    "url": "/archivity/static/media/p2.d858576c.jpg"
+  },
+  {
+    "revision": "7c7d0c01a3c932df6b02f48a39426ff2",
+    "url": "/archivity/static/media/p3.7c7d0c01.jpg"
+  },
+  {
+    "revision": "7a366669cf8fc6ac49994ff896847ad8",
+    "url": "/archivity/static/media/p4.7a366669.jpg"
+  },
+  {
+    "revision": "f2c88145f6187c28a85db45e2814bc59",
+    "url": "/archivity/static/media/p5.f2c88145.jpg"
+  },
+  {
+    "revision": "9e151644fe4e2b7a615d0a9f873ca9e8",
+    "url": "/archivity/static/media/p6.9e151644.jpg"
+  },
+  {
+    "revision": "dd719a89448d3d204b847b53af320a69",
+    "url": "/archivity/static/media/p7.dd719a89.jpg"
+  },
+  {
+    "revision": "6b13f9478f4c400a5a9705e831045841",
+    "url": "/archivity/static/media/p8.6b13f947.jpg"
+  },
+  {
+    "revision": "046e91f23b810b0a45fe47a0297cd6e8",
+    "url": "/archivity/static/media/p9.046e91f2.jpg"
+  },
+  {
+    "revision": "11315dae9f5f5a6c46f4dee17d224aca",
+    "url": "/archivity/static/media/thanks.11315dae.jpg"
+  },
+  {
+    "revision": "470b497fb8360db3361fae2f2efff4ed",
+    "url": "/archivity/static/media/whatsapp.470b497f.png"
+  }
+]);
